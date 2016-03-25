04e6daa917cf82c9a7ac39714c6b1be500d0a5f0
Levi Donaldson <levi@millcreeksoftware.biz>
2016-03-25 09:58:57 -0500

    Merge branch 'master' of bitbucket.org:millcreeksoftware/human_link
    # Conflicts:
    #	account/models.py

--- message/models.py
+++ message/models.py
@@ -4,23 +4,23 @@ from django.db import models
 from account.models import Account
 from org.models import Org
 
-ThreadType = (
+THREADTYPE_CHOICES = (
 	(0, 'Direct'),
 	(1, 'Group')
 )
 
-ThreadPrivacy = (
+PRIVACY_CHOICES = (
 	(0, 'Open'), # see notes file - messages 1
 	(1, 'Closed'),
 	(3, 'Members')
 )
 
-ThreadOwner = (
+OWNER_CHOICES = (
 	(0, 'User'),
 	(1, 'Org')
 )
 
-ChatType = (
+CHAT_CHOICES = (
 	(0, 'Text'),
 	(1, 'System'),
 	(2, 'Joined'),
@@ -32,8 +32,8 @@ ChatType = (
 
 
 class Thread(models.Model):
-	kind = models.IntegerField(null=False)
-	privacy = models.IntegerField(choices=ThreadPrivacy, default=1)
+	kind = models.IntegerField(choices=OWNER_CHOICES, null=False)
+	privacy = models.IntegerField(choices=PRIVACY_CHOICES, default=1)
 	account_id = models.ForeignKey(Account, related_name="thread_account_id", null=True)
 	owner_id = models.ForeignKey(Account, related_name="thread_owner_id")
 	org_id = models.ForeignKey(Org, related_name="thread_org_id")
@@ -43,7 +43,10 @@ class Thread(models.Model):
 
 	@property
 	def owner_kind(self):
-		return self
+		return self.kind
+
+	def get_members(self):
+		return self.threadmember_set.all()
 
 
 class ThreadMember(models.Model):
@@ -51,11 +54,15 @@ class ThreadMember(models.Model):
 	account_id = models.ForeignKey(Account, related_name="threadmember_account_id", null=True)
 	last_seen = models.DateTimeField(default=datetime)
 
+	"""Moves the read cursor to the given timestamp. Default is now."""
+	def update_seen(self, ts=None):
+		self.last_seen = ts
+
 
 class ThreadChat(models.Model):
 	thread_id = models.ForeignKey(Thread, related_name="threadchat_thread_id", on_delete=models.CASCADE, null=False)
 	account_id = models.ForeignKey(Account, related_name="threadchat_account_id", null=True)
-	kind = models.IntegerField(null=False, choices=ChatType, default=0)
+	kind = models.IntegerField(null=False, choices=CHAT_CHOICES, default=0)
 	inviter = models.IntegerField()
 	remover = models.IntegerField()
 	text = models.TextField()
