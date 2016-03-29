a008e73cc3db043801891076d510f8d13de275a1
timothybaney <tim@millcreeksoftware.biz>
2016-03-29 10:34:20 -0500

    Merge branch 'master' of bitbucket.org:millcreeksoftware/human_link
    # Conflicts:
    #	account/models.py

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from api_helpers import ComposeJsonResponse
 from account.models import Account, CareGiver
+from .forms import BasicInfo, CareGiverInfo
 
 # Create your views here.
 
@@ -10,12 +11,12 @@ def index(request):
 
 def me(request):
     """ - Retrieve Current Account Information in JSON Format """
+
     user = request.user
-    account = Account.objects.get(email=user.email)
-    thread = Thread.objects.get(id=account.id)
+    account = Account.objects.get(email=user.email, password=user.password)
 
     context = {
-        "user": user
+        "account": account
     }
 
     return ComposeJsonResponse(200, "", context)
@@ -25,30 +26,48 @@ def update(request):
     """ - Update Account Information """
 
     user = request.user
-    account = Account.objects.get(email=user.email)
+    account = Account.objects.get(email=user.email, password=user.password)
+
+    context = {
+        acc
+    }
 
     if request.POST:
+        form = BasicInfo(request.POST)
+
         cleaned_data = form.cleaned_data
         account.first_name = cleaned_data['first_name']
         account.last_name = cleaned_data['last_name']
         account.phone = cleaned_data['phone']
         account.save()
-    else:
-        pass
 
     return ComposeJsonResponse(200, "", user)
 
 def update_caregiver(request):
-    """Update Caregiver Information
-       -Updates Account's Caregiver Information.
-    """
+    """ -Updates Account's Caregiver Information. """
 
-def profile(request):
+    user = request.user
+    account = Account.objects.get(email=user.email, password=user.password)
+    caregiver = CareGiver.objects.get(account_id=account.id)
+
+    if request.POST:
+        form = CareGiverInfo
+
+        cleaned_data = form.cleaned_data
+        caregiver.is_hireable = cleaned_data['is_hireable']
+        caregiver.location = cleaned_data['location']
+        caregiver.about = cleaned_data['about']
+        caregiver.certs = cleaned_data['certs']
+
+    return ComposeJsonResponse(200, "", context)
+
+
+def profile(request, account_id):
     """Retrieve Profile of the Specified Account
        - Get Account By ID
     """
 
-def caregiver_info(request):
+def caregiver_info(request, account_id):
     """Retrieve Caregiver Details
        - Retrieve Caregiver Details Associated With the Specified Account
     """
