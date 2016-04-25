a0607a35df90a8bf786e7577f978503208a8c4bd
kellygmiller <kellym@millcreeksoftware.biz>
2016-04-25 13:38:35 -0500

    org Admin

--- backend/org/admin.py
+++ backend/org/admin.py
@@ -1,6 +1,36 @@
 from django.contrib import admin
 from org.models import Org, OrgMember, OrgInvite
 
-admin.site.register(Org)
-admin.site.register(OrgMember)
-admin.site.register(OrgInvite)
+
+class OrgMemberInline(admin.TabularInline):
+	model = OrgMember
+	fieldsets = (
+		(None, {
+			'fields': ('account', 'status', 'is_active')
+		}),
+	)
+	raw_id_fields = ('account',)
+
+
+class OrgAdmin(admin.ModelAdmin):
+	list_display = ('name', 'username', 'is_public')
+	raw_id_fields = ('actor',)
+	inlines = (OrgMemberInline, )
+
+
+class OrgMemberAdmin(admin.ModelAdmin):
+	list_filter = ('is_active', 'is_owner', 'is_admin')
+	list_display = ('org', 'account', 'status', 'is_active')
+	search_fields = ('org__name', 'account__email')
+	raw_id_fields = ('org',)
+
+
+class OrgInviteAdmin(admin.ModelAdmin):
+	search_fields = ('name', 'email')
+	list_display = ('org', 'account', 'name', 'email')
+	raw_id_fields = ('org', 'account')
+
+
+admin.site.register(Org, OrgAdmin)
+admin.site.register(OrgMember, OrgMemberAdmin)
+admin.site.register(OrgInvite, OrgInviteAdmin)
